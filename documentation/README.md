c1df74fbe7a7bdd71a4e63a2a6193f1b196ad50b
Delight-In <priyanka.professionalwork@gmail.com>
2025-02-23 18:10:42 -1200

    project template created

---
+++ documentation/README.md
@@ -0,0 +1,27 @@
+### End to End deep learning projects: Chicken-Disease-Classifications
+
+Steps:
+
+1. Problem statement introduction
+2. Github repository setup
+3. Project template creation
+4. Project setup and requirements installation
+5. Creating logging, exception and utils module
+6. Project workflows
+7. Create a all components experiment notebook
+8. Modular code implementations for all components
+9. Create training pipeline
+10. MLops starts
+11. DVC setup for pipeline tracking and implementations
+12. Create prediction pipeline
+13. Create UI for user(app buildng)
+14. Docker creation
+15. CI/CD deployment on cloud(aws, gcs, azure)
+
+
+Prerequisits
+
+1. Python(oops)
+2. DL (object classifications and tensorflow 2.x API)
+3. AWS and AZURE account setup
+
---
+++ template.py
@@ -0,0 +1,43 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s")
+
+main_dir = "src"
+
+temp = [
+    ".github/workflows/.gitkeep",
+    f'{main_dir}/__init__.py',
+    f'{main_dir}/components/__init__.py',
+    f'{main_dir}/utils/__init__.py',
+    f'{main_dir}/pipeline/__init__.py',
+    f'{main_dir}/entity/__init__.py',
+    f'{main_dir}/constants/__init__.py',
+    f'{main_dir}/config/configration/__init__.py',
+    "config/config.yaml",
+    "YAML/dvc.yaml",
+    "YAML/params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "README.md",
+    ".gitignore",
+    "research/notebook.ipynb",
+    "templates/index.html",
+    "static/style.css"
+]
+
+for filpath in temp:
+    filepath = Path(filpath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filename} already exists")
